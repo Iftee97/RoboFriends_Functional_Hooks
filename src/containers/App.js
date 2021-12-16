dbda96a5d9cc1392e13d48cb4adef2fdea0466d7
Iftee1711402 <tawsifhaque97@gmail.com>
2021-12-17 01:13:15 +0600

    added source files

---
+++ src/containers/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState("");
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setRobots(users))
+            .catch(err => console.log("oh no! error:", err));
+    }, []);
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
+    };
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+
+    return !robots.length ? <h1>Loading...</h1> : (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange} />
+            <CardList robots={filteredRobots} />
+        </div>
+    );
+}
+
+export default App;
